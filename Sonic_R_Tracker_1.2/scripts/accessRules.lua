3732196deee7f058c9053a36b68653c3428e342c
halloweeniez <39673487+halloweeniez@users.noreply.github.com>
2024-11-05 22:45:35 -0600

    Removed garbage

--- Sonic_R_Tracker_1.2/scripts/accessRules.lua
+++
@@ -1,6 +0,0 @@
-AccessRules = {
-	["RI"] = function() return HasEnoughToken("RIToken") end,
-    ["RC"] = function() return HasEnoughToken("RCToken") end,
-    ["RR"] = function() return HasEnoughToken("RRToken") end,
-    ["RF"] = function() return HasEnoughToken("RFToken") end,
-}
--- Sonic_R_Tracker_1.2/scripts/logic/logic.lua
+++
@@ -1,5 +0,0 @@
-function HasEnoughToken(target)
-	local token = target .. "Token"
-	local count = Tracker:ProviderCountForCode(token)
-    return count > 4
-end
